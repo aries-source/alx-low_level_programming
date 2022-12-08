e7a75f3d57d3dc108bcd0a330269dcca22d1cb65
aries-source <oforiharrison@outlook.com>
2022-12-08 13:23:34 -0800

    Betty Documentary style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 int main(void)
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 {
 	char c;
 	int i;
