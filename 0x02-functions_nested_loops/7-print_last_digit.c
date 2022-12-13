e03f0bb361345f251fe49ef7efecdf702615cb9a
aries-source <oforiharrison@outlook.com>
2022-12-13 11:59:27 -0800

    Replacing integer

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_last_digit - function that computes the absolute value of an integer
- * @c:  is the int that will use for the argument of the function
+ * @n:  is the int that will use for the argument of the function
  * Return: 0
  */
 int print_last_digit(int n)
