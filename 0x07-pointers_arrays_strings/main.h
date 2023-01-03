eb153a91cfcfec6fd28771285e5a462d7ec3cef3
aries-source <oforiharrison@outlook.com>
2023-01-03 09:56:51 -0800

    main.h updated

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,7 +10,7 @@
 #include <stddef.h>
 
 int _putchar(char c);
-char *_memset(void *s, int c, size_t n);
+char *_memset(char *s, char b, unsigned n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
