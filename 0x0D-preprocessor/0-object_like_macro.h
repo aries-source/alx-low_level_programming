3a137f849153b51c77ee1170f5f517ad853d07e5
aries-source <oforiharrison@outlook.com>
2023-01-16 11:35:07 -0800

    Update2

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,11 +1,4 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
-
-/*
- * Desc: Header file that defines a macro named
- *       SIZE as an abbreviation for the token 1024.
- */
-
+#ifndef _HEADER_
+#define _HEADER_
 #define SIZE 1024
-
-#endif 
+#endif
